cef0a827c46447a424c4ff92c61aa28ad6796045
Shahzad Nizamani <nizamanias124@gmail.com>
2025-09-06 14:58:38 +0500

    everything done

--- app.py
+++ app.py
@@ -16,15 +16,18 @@ def first():
             temps = list(dic.values())
             balls = []
             times = []
+            desc = []
             for temp in temps:
                 balls.append(temp[0])
                 times.append(temp[1])
+                desc.append(temp[2])
+
             amounts = [int(ball)*180 for ball in balls]
         
         if len(names) == 0:
             return render_template("index.html")
 
-        return render_template("index.html", names=names, balls=balls, times=times, amounts=amounts)
+        return render_template("index.html", names=names, balls=balls, desc = desc, times=times, amounts=amounts)
     
     except Exception as e:
         return f"Error: {e}"
@@ -47,33 +50,48 @@ def management():
         dic = json.load(f)
         names = list(dic.keys())
         temps = list(dic.values())
-        balls = []
-        times = []
-        for temp in temps:
-            balls.append(temp[0])
-            times.append(temp[1])
+        balls = [t[0] for t in temps]
+        times = [t[1] for t in temps]
+        desc = [t[2] for t in temps]
         amounts = [ball*180 for ball in balls]
     
     if len(names) == 0:
         return render_template("management.html")
     
     if request.method == "POST":
-        new_name = request.form.get("new_name")
-        new_balls = int(request.form.get("new_balls"))
-        current_time = datetime.now().strftime("%d-%m-%Y %H:%M")
-        dic[new_name] = [new_balls, current_time]
+        action = request.form.get("action")
+        
+        if action == "add":
+            new_name = request.form.get("new_name")
+            new_balls = int(request.form.get("new_balls"))
+            new_desc = request.form.get("new_desc")
+            current_time = datetime.now().strftime("%d-%m-%Y %H:%M")
+            dic[new_name] = [new_balls, current_time, new_desc]
+
+        elif action == "update":
+            name = request.form.get("name")
+            ball = int(request.form.get("balls"))
+            time = datetime.now().strftime("%d-%m-%Y %H:%M")
+            desc = dic[name][2]
+            dic[name] = [ball, time, desc]
+        
+        else:
+            name = request.form.get("name")
+            dic.pop(name)
 
         with open("balls.json", 'w') as f:
            json.dump(dic, f, indent=4)
+        
+        return redirect(url_for("first"))
+           
     names = list(dic.keys())
     temps = list(dic.values())
-    balls = []
-    times = []
-    for temp in temps:
-        balls.append(temp[0])
-        times.append(temp[1])
+    balls = [t[0] for t in temps]
+    times = [t[1] for t in temps]
+    desc = [t[2] for t in temps]
     amounts = [int(ball) * 180 for ball in balls]
-    return render_template("management.html", names=names, balls=balls, times=times, amounts=amounts)
+
+    return render_template("management.html", names=names, balls=balls, desc=desc, times=times, amounts=amounts)
 
 
 if __name__ == "__main__":
